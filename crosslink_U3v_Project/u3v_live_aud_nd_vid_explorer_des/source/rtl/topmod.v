f09990cd9b953c391da17f9d3ffc4ded5ee0b4cb
ethan head <ethan@openwater.cc>
2024-09-05 18:41:06 -0700

    removed mipi reset synchronizer, simplified

--- crosslink_U3v_Project/u3v_live_aud_nd_vid_explorer_des/source/rtl/topmod.v
+++ crosslink_U3v_Project/u3v_live_aud_nd_vid_explorer_des/source/rtl/topmod.v
@@ -1,30 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//	(c) 2020-2021, Cypress Semiconductor Corporation (an Infineon company) or an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-//
-//	This software, including source code, documentation and related materials ("Software") is owned by Cypress Semiconductor Corporation or one of its affiliates
-//	("Cypress") and is protected by and subject to worldwide patent protection (United States and foreign), United States copyright laws and international treaty
-//	provisions.  Therefore, you may use this Software only as provided in the license agreement accompanying the software package from which you obtained this
-//	Software ("EULA").
-//	If no EULA applies, Cypress hereby grants you a personal, non-exclusive, non-transferable license to copy, modify, and compile the Software source code solely
-//	for use in connection with Cypress's integrated circuit products.  Any reproduction, modification, translation, compilation, or representation of this Software
-//	except as specified above is prohibited without the express written permission of Cypress.
-//
-//	Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress reserves the right to make changes to the Software without notice. Cypress does
-//	not assume any liability arising out of the application or use of the Software or any product or circuit described in the Software. Cypress does not authorize
-//	its products for use in any products where a malfunction or failure of the Cypress product may reasonably be expected to result in significant property damage,
-//	injury or death ("High Risk Product"). By including Cypress's product in a High Risk Product, the manufacturer of such system or application assumes all risk of
-//	such use and in doing so agrees to indemnify Cypress against all liability.
-//
-//	Design Name:
-//	Module Name:	topmod
-//	Target Devices:	LIF-MD6000
-//	Description:	This project recevies the MIPI video data from the image sensor and converts it into parallel data.
-// 					The parallel video data is then buffered and sent to SX3 via a SlaveFIFO interface in sockets 0,1 at 83.75 MHz.
-//					It also provides a I2C Slave with internal registers to communicate with SX3.
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//`include "parameters.v"
-
 `timescale 1 ps / 1 ps
 
 module topmod
@@ -56,7 +29,7 @@ module topmod
 	
 	//output pll_lock,
 	output			slwr_o,
-	 output			slrd_o,
+	output			slrd_o,
 	output 			sloe_o,
 	output 			slcs_o,
 	output 			pktend_o,
@@ -257,7 +230,7 @@ endgenerate
 // MIPI IP Reset Manager
 //
 // --------------------------------
-
+/*
  mipi_ip_reset_manager
    #
    (
@@ -274,7 +247,18 @@ endgenerate
     .reset_counter_is_low_o   ( reset_counter_is_low_o ),
     .reset_counter_is_high_o  ( reset_counter_is_high_o )
    );
+*/
+reg mipi_reset_n_o;
 
+  always @ ( posedge clk_osc or negedge reset_n_HFCLKOUT )
+    begin
+      if ( ~reset_n_HFCLKOUT )
+        begin
+          mipi_reset_n_o <= 1'b0;
+        end
+	  else begin mipi_reset_n_o <= 1'b1; end
+  end
+		  
 
 // ODDR to drive GPIF Clock out
 ODDRX1F SX3_CLOCK ( .D0(1'b1), .D1(1'b0), .SCLK(clk_pixel), .RST(1'b0), .Q(slclk_o) );
