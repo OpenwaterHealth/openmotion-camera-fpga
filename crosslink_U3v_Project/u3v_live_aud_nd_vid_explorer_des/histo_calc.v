6a6dbc35700e88b9322cf206b57f2e6f920eb296
ethan head <ethan@openwater.cc>
2024-09-18 01:13:12 -0700

    attept to reintegrate simulated histogram pipeline into lattice code, part 1. lots of warnings, need to use spreadsheet to actually analyze them

--- crosslink_U3v_Project/u3v_live_aud_nd_vid_explorer_des/histo_calc.v
+++ crosslink_U3v_Project/u3v_live_aud_nd_vid_explorer_des/histo_calc.v
@@ -10,7 +10,7 @@ module histogram2 (
     input wire image_done,      // flip this input high when image is finished
     output reg histo_done,      // flips high when histogram calculation is done
     input wire [9:0] bin,       // bin number to read out
-    output reg [23:0] data      // data output of the bin being read
+    output wire [23:0] data      // data output of the bin being read
     );
 
     // Parameters
@@ -18,49 +18,47 @@ module histogram2 (
     parameter READ = 0, WRITE = 1;
 
     // Clock and reset
-    wire mem_clk = fast_clk;
+    //wire mem_clk = fast_clk;
     wire mem_reset = rst;
-    wire mem_write;
-    assign mem_write = fast_clk;
-
-    reg [9:0] mem_addr;
 
     // Histogram output
-    reg [23:0] hist_i;
-    wire [23:0] hist_o;
-
-    always @(posedge fast_clk) begin
-        if(image_done) begin
-            histo_done = 1;
-        end else begin
-            histo_done = 0;
-        end
-    end
-
+    reg [23:0] mem_in;
+    wire [23:0] mem_out;
+    //reg [9:0] mem_addr;
+	wire [9:0] mem_addr = rw ? pixel : bin;
+    
     // RAM module instance
-    ram_dq ram_inst (
-        .Clock(mem_clk),
+    ram_dq_s ram_inst (
+        .Clock(fast_clk),
         .ClockEn(~mem_reset), // Enable clock only when not in reset
         .Reset(mem_reset),
-        .WE(mem_write), // Always write-enable
+        .WE(1'b1), // Always write-enable
         .Address(mem_addr),
-        .Data(hist_i), // Data input 
-        .Q(hist_o) // Output data
+        .Data(mem_in), // Data input 
+        .Q(mem_out) // Output data
     );
-    always @(posedge fast_clk) begin
-        if(rw == WRITE) begin           // if write mode, read and increment the counter
+
+    reg [9:0] prev_pixel;
+    
+    wire rpt = (prev_pixel == pixel);
+    
+    always @(posedge clk) begin 
+        prev_pixel <= pixel;
+    end
+    wire [23:0] incremented_count = mem_out+1;
+
+    assign data = rw ? 24'b0 : mem_out;
+
+    always @(*) begin
+        if(rw) begin           // if write mode, read and increment the counter
             if(pixel_valid) begin
-                mem_addr = pixel;
-                hist_i = hist_o + 1;
+                mem_in = incremented_count;
+                //mem_addr <= pixel;
             end else begin
-                hist_i = hist_o;
-                mem_addr = pixel;    
+                mem_in = mem_out;
             end
-            data = 0;
         end else begin                  // if read mode... 
-            mem_addr = bin;    // increment the address being read
-            //data = hist_o;
-            hist_i = 0;
+            mem_in = 0;
         end
     end
     
@@ -68,26 +66,30 @@ module histogram2 (
     //     mem_addr = 0;
     // end
 
-
     // set up negative edge detection for rw
-    reg rw_prev;
-    always @(posedge fast_clk) begin
-        rw_prev = rw;
-    end
-    wire rw_negedge = (rw==0) && (rw_prev != rw);
+    // reg rw_prev;
+    // always @(posedge fast_clk) begin
+    //     rw_prev = rw;
+    // end
+    // wire rw_negedge = (rw==0) && (rw_prev != rw);
 
-    reg [9:0] prev_mem_addr = 0;
-    reg new_addr;
-    always @(posedge fast_clk) begin
-        new_addr = (mem_addr!= prev_mem_addr);
-        prev_mem_addr = mem_addr;
-    end
-    always @(negedge new_addr) begin
-        data = hist_o;
-    end
+    // reg [9:0] prev_mem_addr = 0;
+    // reg new_addr;
+    // always @(posedge fast_clk) begin
+    //     new_addr = (mem_addr!= prev_mem_addr);
+    //     prev_mem_addr = mem_addr;
+    // end
 
-/*    always @(negedge rw) begin          // on the negative edge of the clock, reset addr to 0
-            /// figure out how to reset the mem address
+    // always @(negedge new_addr) begin
+    //     data <= hist_o;
+    // end
+    
+    
+    always @(posedge fast_clk) begin
+        if(image_done) begin
+            histo_done <= 1;
+        end else begin
+            histo_done <= 0;
+        end
     end
-*/
 endmodule
--- crosslink_U3v_Project/u3v_live_aud_nd_vid_explorer_des/source/rtl/topmod.v
+++ crosslink_U3v_Project/u3v_live_aud_nd_vid_explorer_des/source/rtl/topmod.v
@@ -143,7 +143,7 @@ end
  );
  
 // assign the outputs from the camera to the GPIOS
-assign  mic_clk_o = clk_pixel3;
+//assign  mic_clk_o = uart;
 assign sldata_o = cmos_data;
 assign slrd_o =  cmos_lv;
 assign sloe_o =  cmos_fv;
@@ -155,64 +155,69 @@ wire frame_valid = cmos_fv;           // Frame synchronization signal
 wire [9:0] pixel_data = cmos_data;      // 16-bit bus for pixel data
 wire pixel_valid = frame_valid && line_valid;
 
+wire uart_clk = clk_pixel3;
 
-// Control
-wire histo_done;
-wire serializer_done;
 
-parameter IDLE = 0, IMAGE = 1, HISTO = 2, SERIALIZE = 3;
-reg [1:0] state = 0;
-
-always @(posedge frame_valid, posedge histo_done, posedge serializer_done) begin
-	if(frame_valid && state == IDLE) begin
-		state <= HISTO;
-	end else if (histo_done && state == HISTO) begin
-		state <= SERIALIZE;
-		bin <= 0;
-	end else if (serializer_done && state == SERIALIZE) begin
-		if(bin == 1023) begin
-			state <= IDLE;
-		end else begin
-			bin <= bin + 1;
-		end
-	end
-end
-
-// Histogram generator
-wire [23:0] data; 
-reg [9:0] bin;
-
-histogram2 histo_i (
-	.clk(clk),          // reset - zeros the histogram
-	.fast_clk(fast_clk),
-	.rst(cam_en),          // clock
-	
-	.pixel (pixel_data),  // 10 bit data for each pixel
-	.pixel_valid (pixel_valid),
-	.data(data),       //    when writing, on every rising edge of CLK adds one to the histogram
-	.bin(bin),
-	
-	.rw(frame_valid),           // read/write, when reading outputs histo data/bin num until done
-	.image_done(~frame_valid),
-	.histo_done(histo_done)
-);
-
-// Serializer
-wire uart;
-Serializer seralizer_i (
-	.fast_clk_in(uart_clk),
-	.reset((state != SERIALIZE)),
-	.data_in(data),
-	.serial_out(uart),
-	.slow_clk_out(),
-	.done(serializer_done)
-);
+    // Histogram generator
+    wire [23:0] data; 
+    reg [9:0] bin;
     
+    // Serializer
+    wire uart;
 
+    // Control
+    wire image_done; 
+    wire histo_done;
+    wire serializer_done;
+    
+    parameter IDLE = 0, IMAGE = 1, HISTO = 2, SERIALIZE = 3;
+    reg [1:0] state = 0;
 
+    always @(posedge frame_valid, posedge histo_done, posedge serializer_done) begin
+        if(frame_valid && state == IDLE) begin
+            state <= HISTO;
+        end else if (histo_done && state == HISTO) begin
+            state <= SERIALIZE;
+            bin <= 0;
+        end else if (serializer_done && state == SERIALIZE) begin
+            if(bin == 1023) begin
+                state <= IDLE;
+            end else begin
+                bin <= bin + 1;
+            end
+        end
+    end
 
+    histogram2 histo_i (
+        .clk(clk_pixel3),          // reset - zeros the histogram
+        .fast_clk(clk_pixel_hs),
+        .rst(mipi_reset_n_o),          // clock
+        
+        .pixel (pixel_data),  // 10 bit data for each pixel
+        .pixel_valid (pixel_valid),
+        .data(data),       //    when writing, on every rising edge of CLK adds one to the histogram
+        .bin(bin),
+        
+        .rw(frame_valid),           // read/write, when reading outputs histo data/bin num until done
+        .image_done(~frame_valid),
+        .histo_done(histo_done)
+    );
 
+	reg [23:0] data_persistent;
+    Serializer seralizer_i (
+        .fast_clk_in(clk_pixel),
+        .reset(mipi_reset_n_o),
+        .data_in(data_persistent),
+        .serial_out(mic_clk_o),
+        .slow_clk_out(),
+        .done(serializer_done)
+    );
 
+	wire data_valid = (data != 0);
+	always @(posedge data_valid, posedge serializer_done) begin
+		if(data_valid) data_persistent <= data;
+		else data_persistent <= 24'b0;
+	end
 
 
 
